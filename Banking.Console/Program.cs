3f17656d72141281a81a49da2dca76ae0d7ff755
Gabriel Blauth de Araujo <74154@student.dorset-college.ie>
2024-12-02 15:01:51 +0000

    Update

---
+++ Banking.Console/Program.cs
@@ -0,0 +1,4 @@
+﻿
+Console.WriteLine("Banking Application <A 74154>");
+
+Banking.Driver.Run();
---
+++ Banking.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿namespace Banking.Domain
+{
+    public class BankingDriver
+    {
+        public static void Run();
+
+    }
+}
